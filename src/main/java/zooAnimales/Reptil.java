29ec78bab3626542746e8b8a9c404b1e60af9dcb
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-04-24 10:30:14 -0500

    Update Reptil.java

--- src/main/java/zooAnimales/Reptil.java
+++ src/main/java/zooAnimales/Reptil.java
@@ -15,7 +15,7 @@ public class Reptil extends Animal{
 		listado.add(this);
     }
     public Reptil(String nombre, int edad, String habitat, String genero, Zona zona, String colorEscamas, int largoCola) {
-   	 super(nombre, edad, habitat, genero, zona);
+   	 super(nombre, edad, habitat, genero);
    	 this.colorEscamas=colorEscamas;
    	 this.largoCola=largoCola;
    	 listado.add(this);

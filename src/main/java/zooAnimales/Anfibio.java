1a66e6fc49dda834439e26cf7709a5340e9ac2c6
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-04-24 10:38:25 -0500

    Update Anfibio.java

--- src/main/java/zooAnimales/Anfibio.java
+++ src/main/java/zooAnimales/Anfibio.java
@@ -29,7 +29,7 @@ public class Anfibio extends Animal{
 	public void setVenenoso(boolean venenoso) {
 		this.venenoso=venenoso;
 	}
-	public boolean getVenenoso() {
+	public boolean isVenenoso() {
 		return(venenoso);
 	}
     public static int cantidadAnfibios() {

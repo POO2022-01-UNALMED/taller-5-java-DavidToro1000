317a23e5ba0b22c6ce763e244d53e427d097ef59
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-04-24 10:28:02 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -10,12 +10,11 @@ public class Animal {
 	public Animal() {
 		
 	}
-	public Animal(String nombre, int edad, String habitat, String genero, Zona zona) {
+	public Animal(String nombre, int edad, String habitat, String genero) {
 		this.nombre = nombre;
 		this.edad = edad;
 		this.habitat= habitat;
 		this.genero = genero;
-		this.zona = zona;
 		totalAnimales += 1;
 	}
 	public static int getTotalAnimales() {

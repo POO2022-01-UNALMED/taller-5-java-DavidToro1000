37e339392f0b1c56156079c27d9a8c35ae047507
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-04-24 11:09:17 -0500

    Update Anfibio.java

--- src/main/java/zooAnimales/Anfibio.java
+++ src/main/java/zooAnimales/Anfibio.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import gestion.Zona;
 
 public class Anfibio extends Animal{
-	private static ArrayList<Anfibio> listado;
+	private static ArrayList<Anfibio> listado = new ArrayList<Anfibio>();
     public static int ranas;
     public static int salamandras;
     private String colorPiel;
@@ -14,6 +14,12 @@ public class Anfibio extends Animal{
     	super();
 		listado.add(this);
     }
+    public Anfibio(String nombre, int edad, String habitat, String genero, Zona zona,String colorPiel, boolean venenoso) {
+		super(nombre, edad, habitat, genero, zona);
+		this.colorPiel = colorPiel;
+		this.venenoso = venenoso;
+		listado.add(this);
+	}
     public Anfibio(String nombre, int edad, String habitat, String genero, String colorPiel, boolean venenoso) {
    	 super(nombre, edad, habitat, genero);
    	 this.colorPiel=colorPiel;
